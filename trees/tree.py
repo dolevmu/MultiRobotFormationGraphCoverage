29731df11f495a33294e4ec571a0e38a8d7dfddd
dolevmu <dolevmu@gmail.com>
2024-09-19 03:22:40 +0300

    Print traversal nicely, breaking lines every 4 configs

--- trees/tree.py
+++ trees/tree.py
@@ -6,7 +6,22 @@ def print_tree(tree: Tree, data_property: Optional[str]=None):
     tree_str = tree.show(stdout=False, data_property=data_property)
     print(tree_str)
 
+
 def example_tree():
+    tree = Tree()
+    tree.create_node("Node Root", "")  # root node
+    tree.create_node("Node 0",    "0",    parent="")
+    tree.create_node("Node 1",    "1",    parent="")
+    tree.create_node("Node 00",   "00",   parent="0")
+    tree.create_node("Node 01",   "01",   parent="0")
+    tree.create_node("Node 10",   "10",   parent="1")
+    tree.create_node("Node 100",  "100",  parent="10")
+    tree.create_node("Node 101",  "101",  parent="10")
+    tree.create_node("Node 1010", "1010", parent="101")
+    return tree
+
+
+def hard_example_tree():
     tree = Tree()
     tree.create_node("Node Root", "")  # root node
     tree.create_node("Node 0",    "0",    parent="")
@@ -14,6 +29,12 @@ def example_tree():
     tree.create_node("Node 2", "2", parent="")
     tree.create_node("Node 00",   "00",   parent="0")
     tree.create_node("Node 01",   "01",   parent="0")
+    tree.create_node("Node 010", "010", parent="01")
+    tree.create_node("Node 0100", "0100", parent="010")
+    tree.create_node("Node 011", "011", parent="01")
+    tree.create_node("Node 0110", "0110", parent="011")
+    tree.create_node("Node 0111", "0111", parent="011")
+    tree.create_node("Node 0112", "0112", parent="011")
     tree.create_node("Node 10",   "10",   parent="1")
     tree.create_node("Node 100",  "100",  parent="10")
     tree.create_node("Node 101",  "101",  parent="10")
