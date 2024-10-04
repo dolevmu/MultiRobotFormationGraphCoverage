bb9c0fde9f54a553bf979dc0a37e6c3b9dbe584f
dolevmu <dolevmu@gmail.com>
2024-10-04 16:07:25 +0300

    Added jaxonville hotel

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ from collections import Counter
 from trees.signature import project, compute_signature_cost
 from trees.table import fpt_compute_traversal
 from trees.traversal import print_traversal, is_traversal
-from trees.tree import hard_example_tree, example_tree, print_tree
+from trees.tree import hard_example_tree, example_tree, print_tree, jaxsonville_tree
 
 from time import time
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    tree = hard_example_tree()
+    # tree = hard_example_tree()
+    tree = jaxsonville_tree()
     print_tree(tree)
 
-    start = time()
-    traversal = fpt_compute_traversal(tree, 3)
-    end = time()
-
-    print(f"Traversal Time={len(traversal)}, Tree Size={tree.size()}")
-    print(f"Computation Time={end-start:.2f}")
-    print_traversal(traversal)
+    # start = time()
+    # traversal = fpt_compute_traversal(tree, 3)
+    # end = time()
+    #
+    # print(f"Traversal Time={len(traversal)}, Tree Size={tree.size()}")
+    # print(f"Computation Time={end-start:.2f}")
+    # print_traversal(traversal)
